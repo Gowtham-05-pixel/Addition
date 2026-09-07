975d25a19d1375c71e3719733437d2366c5fe743
Gowtham <hello@123gmail.com>
2026-09-07 20:33:31 +0530

    first commit

---
+++ sum.py
@@ -0,0 +1,11 @@
+def main():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    result = num1 + num2
+
+    print("Sum =", result)
+
+
+if __name__ == "__main__":
+    main()
