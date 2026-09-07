147b6b2159f1b0f4d2bad500c277dccd14d43a70
Gowtham <hello@123gmail.com>
2026-09-07 21:12:42 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calculator():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("Addition =", num1 + num2)
+    print("Subtraction =", num1 - num2)
+    print("Multiplication =", num1 * num2)
+
+    if num2 != 0:
+        print("Division =", num1 / num2)
+    else:
+        print("Division = Cannot divide by zero")
+
+
+if __name__ == "__main__":
+    calculator()
